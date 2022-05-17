35af218ddc647c836b7e1ec695e3772b60209026
Daniil <ldfm0401@gmail.com>
2022-05-17 14:33:00 +0300

    Generate postgres modul from Nest CLI  nest g mo postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostgresModule } from './providers/postgres/postgres.module';
 
 @Module({
-  imports: [],
+  imports: [PostgresModule],
   controllers: [AppController],
   providers: [AppService],
 })
