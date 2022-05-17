9b39a9df5515a224d01a630a3e1284c3cc049a37
Daniil <ldfm0401@gmail.com>
2022-05-17 12:59:03 +0300

    Import and configure Swagger in main.js and then in nest-cli.json

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { INestApplication } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  buildDocumentSwager(app);
   await app.listen(3000);
 }
 bootstrap();
+
+function buildDocumentSwager(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('ToDo')
+    .setDescription('making todo list of user on nest.js')
+    .setVersion('0.0.1')
+    .addTag('todo')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+}
