68f37d3998e420abcfa8639a6d436946d1ac826f
Daniil <ldfm0401@gmail.com>
2022-05-18 03:39:13 +0300

    user creation nest g res User  ? What transport layer do you use? REST API  ? Would you like to generate CRUD entry points? Yes

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostgresModule } from './providers/postgres/postgres.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [PostgresModule],
+  imports: [PostgresModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
