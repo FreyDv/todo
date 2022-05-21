4749a08b42e9cea2cac6bed77aacad4c42aa4ffe
ViktorKosenko <kosenkovv89@gmail.com>
2022-05-21 12:57:58 +0300

    Users::feat: implemented OutputMeUserDto

---
+++ src/modules/users/dto/output-me-user.dto.ts
@@ -0,0 +1,19 @@
+import { UserEntity } from '../entities/user.entity';
+
+export class OutputMeUserDto {
+  id: number;
+  name: string;
+  privateField: string;
+
+  static fromUserEntity({
+    id,
+    name,
+    privateField,
+  }: UserEntity): OutputMeUserDto {
+    return {
+      id,
+      name,
+      privateField,
+    };
+  }
+}
