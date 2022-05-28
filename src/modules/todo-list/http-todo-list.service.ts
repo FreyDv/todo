e56f397b75c97ef6908306edba4475609549f9eb
Daniil <ldfm0401@gmail.com>
2022-05-28 10:27:51 +0300

    TodoList::feat: created HttpTodoListService.findAllByUserId

--- src/modules/todo-list/http-todo-list.service.ts
+++ src/modules/todo-list/http-todo-list.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import {TodoListService} from "./todo-list.service";
 import {CreateTodoListDto} from "./dto/create-todo-list.dto";
 import {OutputTodoListDto} from "./dto/output-todo-list.dto";
+import {EntityNotFoundException} from "../../common/exceptions/entity-not-found.exception";
 
 @Injectable()
 export class HttpTodoListService {
@@ -10,5 +11,16 @@ export class HttpTodoListService {
     async create(createTodoListDto: CreateTodoListDto):Promise<OutputTodoListDto>{
         let result = await this.todoListService.create(createTodoListDto);
         return OutputTodoListDto.fromTodoListEntity(result);
+    }
+
+    async findAllByUserId(id: number):Promise<OutputTodoListDto[]>{
+        let result = await this.todoListService.findAllByUserId(id);
+        if(!result){
+            throw new EntityNotFoundException('Cant find eny ToDo for this Users');
+        }
+        result.map((todo)=>{
+            return OutputTodoListDto.fromTodoListEntity(todo);
+        })
+        return result;
     }
 }
