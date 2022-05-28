7ac8459cdaec197621c9d3ed3d5b3beda3476942
Daniil <ldfm0401@gmail.com>
2022-05-28 05:13:14 +0300

    users::feat: added in  HttpUsersService method findll

--- src/modules/users/http-users.service.ts
+++ src/modules/users/http-users.service.ts
@@ -2,6 +2,8 @@ import {Injectable} from "@nestjs/common";
 import {UsersService} from "./users.service";
 import {CreateUserDto} from "./dto/create-user.dto";
 import {OutputUserDto} from "./dto/output-user.dto";
+import {EntityNotFoundException} from "../../common/exceptions/entity-not-found.exception";
+import {OutputMeUserDto} from "./dto/output-me-user.dto";
 
 @Injectable()
 export class HttpUsersService{
@@ -10,5 +12,14 @@ export class HttpUsersService{
     async create(createUserDto: CreateUserDto): Promise<OutputUserDto>{
         let result = await this.usersService.create(createUserDto)
         return OutputUserDto.fromUserEntity(result);
+    }
+    async findAll(): Promise<OutputUserDto[]>{
+        let result = await this.usersService.findAll()
+        if(!result){
+            throw new EntityNotFoundException('No one found');
+        }
+        return result.map((user)=>{
+            return OutputUserDto.fromUserEntity(user);
+        });
     }
 }
