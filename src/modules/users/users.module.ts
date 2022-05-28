6c6c7a1b206e44bd2f164efec809dd978449a339
ViktorKosenko <kosenkovv89@gmail.com>
2022-05-28 21:02:01 +0300

    Users::imported ConfigModule

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import usersConfig from './config/user.config';
 import { UserEntity } from './entities/user.entity';
 import { HttpUsersService } from './http-users.service';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [
+    TypeOrmModule.forFeature([UserEntity]),
+    ConfigModule.forRoot({ load: [usersConfig] }),
+  ],
   controllers: [UsersController],
   providers: [UsersService, HttpUsersService],
   exports: [UsersService],
