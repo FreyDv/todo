168f60b0589a7fc120a5b61a43d3ceee969f032e
Daniil <ldfm0401@gmail.com>
2022-05-28 03:00:44 +0300

    users::feat: added method create in HttpUsersService

--- src/modules/users/http-users.service.ts
+++ src/modules/users/http-users.service.ts
@@ -1,9 +1,14 @@
 import {Injectable} from "@nestjs/common";
 import {UsersService} from "./users.service";
-
+import {CreateUserDto} from "./dto/create-user.dto";
+import {OutputUserDto} from "./dto/output-user.dto";
 
 @Injectable()
 export class HttpUsersService{
     constructor(private readonly usersService: UsersService) {}
 
+    async create(createUserDto: CreateUserDto): Promise<OutputUserDto>{
+        let result = await this.usersService.create(createUserDto)
+        return OutputUserDto.fromUserEntity(result);
+    }
 }
