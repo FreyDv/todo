540ef20291258fc8ab3801ce123e4634409ae864
Daniil <ldfm0401@gmail.com>
2022-05-28 09:59:21 +0300

    TodoList::feat: create http-todo-list.service.ts

--- src/modules/todo-list/todo-list.module.ts
+++ src/modules/todo-list/todo-list.module.ts
@@ -4,10 +4,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TodoListEntity } from './entities/todo-list.entity';
 import { TodoListController } from './todo-list.controller';
 import { TodoListService } from './todo-list.service';
+import { HttpTodoListService } from './http-todo-list.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([TodoListEntity])],
   controllers: [TodoListController],
-  providers: [TodoListService],
+  providers: [TodoListService, HttpTodoListService],
 })
 export class TodoListModule {}
