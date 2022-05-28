47e40655fb4b5d45097e8755e43193aa0bff8dfe
Daniil <ldfm0401@gmail.com>
2022-05-28 09:58:31 +0300

    TodoList::feat: added OutputTodoListDto.fromTodoListEntity method

--- src/modules/todo-list/dto/output-todo-list.dto.ts
+++ src/modules/todo-list/dto/output-todo-list.dto.ts
@@ -1,5 +1,15 @@
+import {TodoListEntity} from "../entities/todo-list.entity";
+
 export class OutputTodoListDto {
   id: number;
   title: string;
   isDone: boolean;
+
+  static fromTodoListEntity({id,title,isDone}:TodoListEntity):OutputTodoListDto{
+    return {
+      id,
+      title,
+      isDone
+    }
+  }
 }
