1fd17f578228ff3eaaf25df49d7535ea915ea511
Daniil <ldfm0401@gmail.com>
2022-05-28 10:26:41 +0300

    TodoList::feat: created HttpTodoListService.create

--- src/modules/todo-list/http-todo-list.service.ts
+++ src/modules/todo-list/http-todo-list.service.ts
@@ -1,7 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import {TodoListService} from "./todo-list.service";
+import {CreateTodoListDto} from "./dto/create-todo-list.dto";
+import {OutputTodoListDto} from "./dto/output-todo-list.dto";
 
 @Injectable()
 export class HttpTodoListService {
     constructor(private readonly todoListService: TodoListService) {}
+
+    async create(createTodoListDto: CreateTodoListDto):Promise<OutputTodoListDto>{
+        let result = await this.todoListService.create(createTodoListDto);
+        return OutputTodoListDto.fromTodoListEntity(result);
+    }
 }
