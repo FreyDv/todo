8bce44b9dd6efb178c6c2eca9a4b9210f56b6558
Daniil <ldfm0401@gmail.com>
2022-06-04 01:16:39 -0500

    User:feat::added email, password in CreateUserDto

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,5 +1,5 @@
 // import { Type } from 'class-transformer';
-import { Allow, IsString } from 'class-validator';
+import { Allow, IsEmail, IsString, MinLength } from 'class-validator';
 
 // class MetaCreateUserDto {
 //   @IsArray()
@@ -20,6 +20,12 @@ export class CreateUserDto {
   @Allow()
   privateField: string;
 
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(4)
+  password: string;
   // @Allow()
   // @ValidateNested()
   // @Type(() => MetaCreateUserDto)
