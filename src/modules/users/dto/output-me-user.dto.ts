5de7330f8cbed8707ce4048f51406a37555b2cb7
Daniil <ldfm0401@gmail.com>
2022-06-04 01:07:46 -0500

    User:feat::added email in OutputMeUserDto

--- src/modules/users/dto/output-me-user.dto.ts
+++ src/modules/users/dto/output-me-user.dto.ts
@@ -4,16 +4,19 @@ export class OutputMeUserDto {
   id: number;
   name: string;
   privateField: string;
+  email: string;
 
   static fromUserEntity({
     id,
     name,
     privateField,
+    email,
   }: UserEntity): OutputMeUserDto {
     return {
       id,
       name,
       privateField,
+      email,
     };
   }
 }
