b45e78f275190a07f36e00ba1ab0795c29c7ecc3
Daniil <ldfm0401@gmail.com>
2022-06-18 15:35:44 -0500

    Account:fix::methods CurrentUserAuth

--- src/common/decorators/current-user-auth.decorator.ts
+++ src/common/decorators/current-user-auth.decorator.ts
@@ -1,9 +1,6 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
-export const CurrentUserAuth = createParamDecorator(
-  (keyName: string, ctx: ExecutionContext) => {
-    const req = ctx.switchToHttp().getRequest();
-
-    return keyName ? req.user?.[keyName] : req.user;
-  },
-);
+export const CurrentUserAuth = createParamDecorator((keyName: string, ctx: ExecutionContext) => {
+  const req = ctx.switchToHttp().getRequest();
+  return keyName ? req.user?.[keyName] : req.user;
+});
