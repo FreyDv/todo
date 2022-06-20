abbfeb1e8a4a485c8ac167acacdc7204d92b1fa3
Daniil <ldfm0401@gmail.com>
2022-06-20 05:26:29 -0500

    Mail:feat::create method to send mail in MailService

--- src/modules/mail/mail.service.ts
+++ src/modules/mail/mail.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as SendGrid from '@sendgrid/mail';
+
+import { SendEmailDto } from './dto/send-email.dto';
 
 @Injectable()
-export class MailService {}
+export class MailService {
+  public constructor(private readonly conficSeervice: ConfigService) {
+    SendGrid.setApiKey(this.conficSeervice.get<string>('SENDGRID_API_KEY') || '');
+  }
+
+  async send(sendEmailDto: SendEmailDto) {
+    const mail = {
+      to: sendEmailDto.email,
+      subject: 'email confirmation',
+      from: 'ldfm0401@gmail.com',
+      text: sendEmailDto.msg,
+    };
+    const transport = await SendGrid.send(mail);
+    console.log(`Email successfully dispatched to ${mail.to}`);
+    return transport;
+  }
+}
