61c956818e3b1e6785db0a0399f025810ccf8477
Daniil <ldfm0401@gmail.com>
2022-06-20 04:28:19 -0500

    Mail:feat::import and configuration SendGridModule

--- src/modules/mail/mail.module.ts
+++ src/modules/mail/mail.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SendGridModule } from '@ntegral/nestjs-sendgrid';
 
 import { MailController } from './mail.controller';
 import { MailService } from './mail.service';
 
 @Module({
+  imports: [
+    SendGridModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (cfg: ConfigService) => ({
+        apiKey: cfg.get<string>('SENDGRID_API_KEY') || 'as',
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   providers: [MailService],
   controllers: [MailController],
 })
