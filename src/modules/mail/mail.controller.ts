b06d32dc2af026a906436ff8d22ad825d73922ce
Daniil <ldfm0401@gmail.com>
2022-06-20 05:27:21 -0500

    Mail:feat::create end-Point in MailController for send mail

--- src/modules/mail/mail.controller.ts
+++ src/modules/mail/mail.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import * as Swagger from '@nestjs/swagger';
 
+import { SendEmailDto } from './dto/send-email.dto';
+import { MailService } from './mail.service';
+
+@Swagger.ApiTags('Mail')
 @Controller('mail')
-export class MailController {}
+export class MailController {
+  constructor(private readonly mailService: MailService) {}
+
+  @Post()
+  sendEmail(@Body() mail: SendEmailDto) {
+    return this.mailService.send(mail);
+  }
+}
