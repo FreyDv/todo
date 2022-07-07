fd943c712f38734a840a98219a01e70ed4d942f6
Daniil <ldfm0401@gmail.com>
2022-07-07 14:25:05 -0500

    Account:feat::added validateMail, findAccountByEmail, findAccountsByEmail, googleLogin, updateAccount, findUserIfEmailAlreadyExists, findMsgInMemory    motherhood and add sendValidationMail

--- src/modules/account/account.service.ts
+++ src/modules/account/account.service.ts
@@ -56,8 +56,130 @@ export class AccountService {
       } else throw new WrongCredentialsProvidedException();
     } else throw new WrongCredentialsProvidedException();
   }
-
-  getJwtWithId(userId: number): string {
+  async validateMail(msg: string) {
+    const inMemory = this.findMsgInMemory(msg);
+    if (!inMemory) {
+      throw new Error('The message not found');
+    }
+    const resultOfCompare = await this.bcrypt.comparePassword(msg, inMemory.id + '' + inMemory.msFromUnix);
+    if (resultOfCompare) {
+      const isSuccess = this.updateVerifiedAccount(inMemory.id, true);
+      if (!isSuccess) throw new Error('Cannot verified email');
+      console.log(`Успешно провалидирован акаунт c id:${inMemory.id}`);
+      const account = await this.findByID(inMemory.id);
+      if (account) {
+        const userAlreadyExists = await this.findUserIfEmailAlreadyExists(account);
+        if (userAlreadyExists) {
+          await this.create(account, undefined, userAlreadyExists);
+        }
+      }
+      return { success: true };
+    } else throw new Error('Error comparing secret');
+  }
+  async findAccountByEmail(email: string, type: AuthProvider = AuthProvider.Local): Promise<AccountEntity> {
+    const account = await this.authRepository.find({
+      where: {
+        email: email,
+        type: type,
+      },
+      relations: ['user'],
+    });
+    return account[0];
+  }
+  private async findAccountsByEmail(email: string): Promise<AccountEntity[]> {
+    const account = await this.authRepository.find({
+      where: {
+        email: email,
+        verified: true,
+      },
+      relations: ['user'],
+    });
+    return account;
+  }
+  async googleLogin(req) {
+    if (!req.user) throw new Error('Can not get User from Google!');
+    let account: AccountEntity;
+    const userGoogle = req.user._json;
+    account = await this.findAccountByEmail(userGoogle.email, AuthProvider.Google);
+    if (account) {
+      if (account.user && account.verified) {
+        return account;
+      }
+    } else {
+      const resultCreating = await this.create(new AuthDto(userGoogle.email), AuthProvider.Google);
+      if (!resultCreating) throw new Error('Can not create account!');
+      account = resultCreating;
+    }
+    const userAlreadyExists = await this.findUserIfEmailAlreadyExists(account);
+    if (userAlreadyExists) {
+      account.user = userAlreadyExists;
+      return await this.updateAccount(account);
+    }
+  }
+  updateAccount(account: AccountEntity) {
+    return this.authRepository.save(account);
+  }
+  async findUserIfEmailAlreadyExists(account: AccountEntity): Promise<UserEntity | undefined> {
+    const accountWithSameEmail = await this.findAccountsByEmail(account.email);
+    for (const acc of accountWithSameEmail) {
+      if (acc.user && acc.type != account.type) {
+        return acc.user;
+      }
+    }
+    return undefined;
+  }
+  private findMsgInMemory(msg: string) {
+    const forDelete = new Array<number>();
+    const nowD = Date.now();
+    const inMemory = this.memoryOfSendingValidationMessage.find((el, index) => {
+      if (el.msFromUnix > nowD) {
+        if (el.msg === msg) {
+          forDelete.push(index);
+          return true;
+        }
+      } else forDelete.push(index);
+    });
+    if (!inMemory) throw new Error('Запись не найдена');
+    // Очистка масива от сообшений которые были подтвержденны или истекли по сроку давности!
+    if (forDelete.length > 0) {
+      this.memoryOfSendingValidationMessage.filter((elm, indexMemory) => {
+        for (const indexForDelete of forDelete) {
+          if (indexForDelete === indexMemory) return false;
+        }
+        return true;
+      });
+    }
+    return inMemory;
+  }
+  private async create(
+    authDto: AuthDto,
+    type: AuthProvider = AuthProvider.Local,
+    user?: UserEntity,
+  ): Promise<AccountEntity | undefined> {
+    let verified = true;
+    if (type == AuthProvider.Local) {
+      verified = false;
+    }
+    return this.authRepository.save({
+      ...authDto,
+      type,
+      verified,
+      user,
+    });
+  }
+  async findByID(id: number): Promise<AccountEntity | undefined> {
+    return await this.authRepository.findOne(id);
+  }
+  private async updateVerifiedAccount(id: number, verified: boolean) {
+    const account = await this.authRepository.findOne(id);
+    if (!account) throw new Error('User with this id does not exist');
+    account.verified = verified;
+    const account2 = await this.authRepository.save(account);
+    if (!account2) throw new Error('User with this id does not exist');
+    if (account2.verified !== verified) throw new Error('Cannot verified email');
+    return true;
+  }
+  private getJwtWithId(userId: number): string {
     const payload = { id: userId };
     return this.jwtService.sign(payload);
   }
