7fa2a6cde68f0599335c8324bf0e637ac472a9c6
Daniil <ldfm0401@gmail.com>
2022-07-24 02:11:32 -0500

    Redis-Cash:feat::created RedisCashModule

---
+++ src/modules/redis-cacsh/redis-cash.module.ts
@@ -0,0 +1,20 @@
+import { CacheModule, Module } from '@nestjs/common';
+import * as redisStore from 'cache-manager-redis-store';
+
+import { RedisCashService } from './redis-cash.service';
+
+@Module({
+  imports: [
+    CacheModule.registerAsync({
+      useFactory: () => ({
+        store: redisStore,
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT,
+        ttl: 86400,
+      }),
+    }),
+  ],
+  providers: [RedisCashService],
+  exports: [RedisCashService],
+})
+export class RedisCashModule {}
